4a271802eb38975abafdf633464eb2e3c8879fac
SV Rancher <4hwwmuse@gmail.com>
2015-09-02 17:22:55 -0700

    Update models.py
    Thanks for building this package.  I'm proposing a fix for the "Cannot Serialize" issue that arises when using django-dropbox in Django 1.8 by using deconstructible.  I'm not sure what impact this might have on older versions of Django, but it works for me now in Django 1.8.3.

--- django_dropbox_project/dropbox_testing/models.py
+++ django_dropbox_project/dropbox_testing/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.utils.deconstruct import deconstructible
 from django_dropbox.storage import DropboxStorage
 
-STORAGE = DropboxStorage()
+# If you are using django 1.8, you'll get a "Cannot Serialize" error.  A quick workaround is:
+@deconstructible
+class MyDropboxStorage(DropboxStorage):
+    pass
+
+STORAGE = MyDropboxStorage()
 
 class Person(models.Model):
      photo = models.ImageField(upload_to='photos', storage=STORAGE, null=True, blank=True)
